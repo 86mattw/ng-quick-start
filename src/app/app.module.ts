989ef4b061553ceac9692798b4c29f9509dc3bd1
MattWilson <matt.wilson@immediate.co.uk>
2016-10-21 14:18:30 +0100

    Added in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }               from '@angular/core';
 import { BrowserModule }          from '@angular/platform-browser';
 import { FormsModule }            from '@angular/forms';
-import { RouterModule }           from '@angular/router';
+import { HttpModule }             from '@angular/http';
 
 import { AppComponent }           from './app.component';
 import { DashboardComponent }     from './dashboard/dashboard.component';
@@ -11,10 +11,16 @@ import { HeroService }            from './services/hero.service';
 
 import { AppRoutingModule }       from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule }   from 'angular-in-memory-web-api/in-memory-web-api.module';
+import { InMemoryDataService }    from './in-memory-data.service';
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
@@ -23,9 +29,7 @@ import { AppRoutingModule }       from './app-routing.module';
     HeroListComponent,
     HeroDetailComponent
   ],
-  providers: [
-    HeroService
-  ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,12 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable }         from '@angular/core';
+import { Headers, Http }      from '@angular/http';
 
-import { Hero } from '../models/hero';
-import { HEROES } from '../data/heroes';
+import 'rxjs/add/operator/toPromise';
+
+import { Hero }               from '../models/hero';
+
+// you are here: https://angular.io/docs/ts/latest/tutorial/toh-pt6.html
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'app/heroes';
+
+  constructor(private http: Http) {}
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(res => res.json().data as Hero[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.log('An error occurred', error);
+    return Promise.reject(error.message || error);
   }
 
   getHero(id: number): Promise<Hero> {
